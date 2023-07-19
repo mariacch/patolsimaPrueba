e62f61c7cf9d27e4f06c47c9ba2305bddfc16ba1
Maria Carrero <mariacarrerochacon267@gmail.com>
2023-07-19 18:19:31 -0400

    first commir

---
+++ src/Inicio.jsx
@@ -0,0 +1,9 @@
+function Inicio() {
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Esta es la páfhgina de inicio</h1>
+    </div>
+  );
+}
+
+export default Inicio;
---
+++ src/SobreNosotros.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SobreNosotros() {
+  return (
+    <div>
+      <h1>Esta es la página sobre nosotros</h1>
+    </div>
+  );
+}
+
+export default SobreNosotros;
